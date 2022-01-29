71e35dcd469883882faea86d93fa868161c62b5e
Andrés Pájaro <pajaroandres44@gmail.com>
2022-01-29 14:17:24 -0500

    Basic window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Card {
+  cardNumber: string
+  owner: string,
+  documentId: string
+  phone: string,
+  status: string,
+  type: string
+}
+
+interface Transaction {
+  responseCode: string,
+  message : string,
+  transactionStatus : string,
+  referenceNumber : string,
+  transactionDate: Date,
+  total: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +25,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CardFront';
+
+  card : Card = {
+    cardNumber: '',
+    owner: '',
+    documentId: '',
+    phone: '',
+    status: '',
+    type: ''
+  };
+  transactions: Transaction[] = [];
+  searchText: string = "";
+
+  constructor(private http: HttpClient) { }
+
+  searchCard() {
+    this.http.get<Card>("http://127.0.01:8080/card/"+this.searchText)
+      .subscribe(res => { 
+        console.log(res) 
+        this.card = res;
+      }, err => console.log(err)
+      );
+    this.searchTransactions();  
+  }
+
+  searchTransactions() {
+    this.http.get<Transaction[]>("http://127.0.01:8080/transaction/"+this.searchText)
+      .subscribe(res => { 
+        console.log(res) 
+        this.transactions = res;
+      }, err => console.log(err)
+      );
+  }
+
+  ngOnInit(): void {
+    console.log("App start!");
+    
+  }
 }
